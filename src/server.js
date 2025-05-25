dbdf775bebef68ab9623ebb0301d41713e42a683
camom11e <49825585+camom11e@users.noreply.github.com>
2025-05-25 22:38:58 +0300

    Добавил файлы REAMDI.m и dev.env

--- src/server.js
+++ src/server.js
@@ -30,4 +30,4 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log(`Server running on port: ${process.env.PORT}`));
